52e508e0ab76350d3fe4d807893f361443180d4d
saim-x <x_aci@hotmail.com>
2024-08-16 16:35:27 +0500

    ddd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 import Image from "next/image";
 import { Images } from "lucide-react";
 import { CiImageOn } from "react-icons/ci";
 import { useRef, useState } from "react";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import convertor from "@/lib/convertor";
 import TextCard from "@/components/Cards/TextCard";
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
 
   const openBrowse = () => {
     imgInputRef.current?.click();
-  }
+  };
   const convert = async (url: string) => {
     if (url) {
       setProcessing(true);
@@ -27,7 +27,7 @@ export default function Home() {
       });
       setProcessing(false);
     }
-  }
+  };
   return (
     <div>
       <h1 className="text-white text-4xl md:text-6xl text-center px-5 pt-5 font-[800] ">
@@ -36,17 +36,32 @@ export default function Home() {
           Tesseract Js{" "}
         </span>
       </h1>
-      <input type="file" required ref={imgInputRef} hidden onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-          const url: string = URL.createObjectURL(e.target.files[0]);
+      <input
+        type="file"
+        required
+        ref={imgInputRef}
+        hidden
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+
+          const url: string = URL.createObjectURL(e.target.files?.[0]!);
           convert(url);
-        }
 
-      }} />
-
+        }}
+      />
 
       <div className="w-full md:p-20 p-5 flex items-center justify-center">
-        <div onClick={openBrowse} className="min-h-[50vh] cursor-pointer  bg-[#2c2c2c] rounded-xl w-full md:p-20 p-5 flex items-center justify-center">
+        <div
+          onClick={openBrowse}
+          onDrop={(e: any) => {
+            e.preventDefault();
+            const url = URL.createObjectURL(e.dataTransfer.files?.[0]!);
+            convert(url);
+          }}
+          onDragOver={(e:any) => {
+            e.preventDefault();
+          }}
+          className="min-h-[50vh] cursor-pointer  bg-[#2c2c2c] rounded-xl w-full md:p-20 p-5 flex items-center justify-center"
+        >
           <div className="flex items-center justify-center flex-col gap-2">
             <p className="text-center text-2xl font-bold text-[#777777]">
               {processing ? "Processing..." : "Click or drop image here"}
@@ -59,14 +74,10 @@ export default function Home() {
         </div>
       </div>
 
-
       <div className="my-10 md:px-20 px-5 space-y-10">
-        {
-          texts.map((t, i) => {
-            return <TextCard key={i} i={i} t={t} />
-          })
-        }
-
+        {texts.map((t, i) => {
+          return <TextCard key={i} i={i} t={t} />;
+        })}
       </div>
     </div>
   );
